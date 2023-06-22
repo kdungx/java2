c8f878a7f08c1c26fab826104e012bca118a1b65
kdungx <dungkath2209058@fpt.edu.vn>
2023-06-22 15:56:38 +0700

    .

---
+++ src/crm/Main.java
@@ -0,0 +1,8 @@
+package crm;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomerRelationshipManagement crm = new CustomerRelationshipManagement();
+        crm.run();
+    }
+}
